d355da10785fcb6f6f936f9b97e5e246cdeeb4fc
mine151015 <mine.151015@gmail.com>
2022-03-15 19:17:17 -0500

    iniciando proyecto de api

---
+++ app.js
@@ -0,0 +1,12 @@
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const app = express()
+
+app.use(cors())
+
+const port = process.env.PORT || 3000
+
+app.listen(port, ()=>{
+    console.log(`http://localhost:${port}`)
+})
