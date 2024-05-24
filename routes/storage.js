b787749a4437cc0c380aa5abeb33253e05aa64f1
Elias Mercado <eliasmercan@msn.com>
2024-05-24 17:23:56 -0500

    Agregando cambios a el almacenamiento

--- routes/storage.js
+++ routes/storage.js
@@ -1,16 +1,31 @@
 
+const multer = require('multer')
 const express = require("express")
 const router = express.Router()
-const uploadMiddleware = require("../utils/handleStorage");
+// const uploadMiddleware = require("../utils/handleStorage");
 const { createItem } = require("../controllers/storage")
 
-
-
 //TODO http://localhost:3001/api/storage
+// router.post("/", uploadMiddleware.single("myfile"), createItem)
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const pathStorage = `${__dirname}/../storage`;
+        cb(null, pathStorage)
+    },
+    filename: function (req, file, cb) {
+        const ext = file.originalname.split('.').pop();
+        const filename = `file-${Date.now()}.${ext}`;
+        cb(null, filename)
+    },
+});
+
+const uploadMiddleware = multer({storage});
 
 
+router.post("/",uploadMiddleware.single("myfile"), (req, res)=>{
+    res.send({a:1})
+})
 
-
-router.post("/", uploadMiddleware.single("myfile"), createItem)
 
 module.exports = router;
